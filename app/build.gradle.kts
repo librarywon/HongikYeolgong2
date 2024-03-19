507f43415763c97960ab765932d757e787dad6c5
librarywon <librarywon@naver.com>
2024-03-19 15:33:23 +0900

    feat: add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,5 +41,26 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // Retrofit2
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
 
+    // Kotlinx Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+
+    // Okhttp3
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+    // ViewPager2
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+
+    // coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
+    // fragment-ktx
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
+
+    // viewModel
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 }
