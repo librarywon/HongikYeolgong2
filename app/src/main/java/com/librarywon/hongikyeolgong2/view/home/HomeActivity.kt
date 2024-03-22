e169471184c061d0e48a3cc06bd3b988ac959bcb
librarywon <librarywon@naver.com>
2024-03-22 13:23:08 +0900

    feat: add Home, Menu Activity

---
+++ app/src/main/java/com/librarywon/hongikyeolgong2/view/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.librarywon.hongikyeolgong2.view.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.librarywon.hongikyeolgong2.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
